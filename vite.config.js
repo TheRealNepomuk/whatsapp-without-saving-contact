391493415945300a69b0afa64a39bcb2ba3341db
TheRealNepomuk <54232605+TheRealNepomuk@users.noreply.github.com>
2025-09-07 20:12:29 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/whatsapp-without-saving-contact/', // 👈 add this line
   server: {
     host: true, // listen on all addresses, including LAN
     port: 5173,
